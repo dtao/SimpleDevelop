b31b773865c4433749604914d33951e6ab899e8c
Dan Tao <daniel.tao@gmail.com>
2013-03-30 14:21:44 -0700

    changed ExecuteWithCallback so that it includes program output

--- SimpleDevelop.Core/CodeDomExecutor.cs
+++ SimpleDevelop.Core/CodeDomExecutor.cs
@@ -27,6 +27,47 @@ namespace SimpleDevelop.Core
 
         public void Execute(IEnumerable<string> code)
         {
+            CompilerResults results = Compile(code);
+
+            if (results.Errors.Count < 1)
+            {
+                Process.Start(results.PathToAssembly);
+            }
+        }
+
+        public void ExecuteWithCallback(string code, Action<string> callback)
+        {
+            var output = new StringBuilder();
+            var appendBuildOutput = new EventHandler<BuildOutputEventArgs>((sender, e) =>
+            {
+                output.AppendLine(e.Message);
+            });
+
+            this.BuildOutput += appendBuildOutput;
+            CompilerResults results = Compile(new string[] { code });
+            this.BuildOutput -= appendBuildOutput;
+
+            var processStartInfo = new ProcessStartInfo
+            {
+                CreateNoWindow = true,
+                FileName = results.PathToAssembly,
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+
+            using (Process p = Process.Start(processStartInfo))
+            {
+                while (!p.StandardOutput.EndOfStream)
+                {
+                    output.AppendLine(p.StandardOutput.ReadLine());
+                }
+            }
+
+            callback(output.ToString());
+        }
+
+        protected CompilerResults Compile(IEnumerable<string> code)
+        {
             var options = new CompilerParameters
             {
                 GenerateExecutable = true,
@@ -51,26 +92,9 @@ namespace SimpleDevelop.Core
                 {
                     OnBuildOutput(results.Errors[i].ErrorText);
                 }
-
-                return;
             }
 
-            Process.Start(results.PathToAssembly);
-        }
-
-        public void ExecuteWithCallback(string code, Action<string> callback)
-        {
-            var output = new StringBuilder();
-            var appendBuildOutput = new EventHandler<BuildOutputEventArgs>((sender, e) =>
-            {
-                output.AppendLine(e.Message);
-            });
-
-            this.BuildOutput += appendBuildOutput;
-            this.Execute(new string[] { code });
-            this.BuildOutput -= appendBuildOutput;
-
-            callback(output.ToString());
+            return results;
         }
 
         protected abstract CodeDomProvider CreateProvider();
