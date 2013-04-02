51d432a3ffb84bf33b0415154882bf48b2f35918
Dan Tao <daniel.tao@gmail.com>
2013-04-02 07:11:26 -0700

    greatly simplified MonoMac version to just use new SimpleDevelop.Application class

--- SimpleDevelop.Mac/MainWindowController.cs
+++ SimpleDevelop.Mac/MainWindowController.cs
@@ -12,7 +12,7 @@ namespace SimpleDevelop.Mac
 {
     public partial class MainWindowController : MonoMac.AppKit.NSWindowController
     {
-        BackgroundWorker backgroundWorker;
+        Application app;
         
         #region Constructors
         
@@ -38,9 +38,9 @@ namespace SimpleDevelop.Mac
         // Shared initialization code
         void Initialize()
         {
-            backgroundWorker = new BackgroundWorker();
-            backgroundWorker.DoWork += HandleDoWork;
-            backgroundWorker.RunWorkerCompleted += HandleRunWorkerCompleted;
+            this.app = new Application();
+            this.app.Start();
+            this.app.Stopped += HandleApplicationStopped;
         }
         
         #endregion
@@ -56,74 +56,12 @@ namespace SimpleDevelop.Mac
         
         public override void WindowDidLoad()
         {
-            // TODO: Get this to work properly with embedded resources.
-            var directory = new FileInfo(typeof(MainWindowController).Assembly.Location).Directory;
-            while (directory.Name != "Release" && directory.Name != "Debug")
-            {
-                directory = directory.Parent;
-            }
-            
-            string resourcesDirectoryPath = Path.Combine(directory.FullName, "Resources");
-            string html = File.ReadAllText(Path.Combine(resourcesDirectoryPath, "editor.html"));
-            string css = File.ReadAllText(Path.Combine(resourcesDirectoryPath, "codemirror.css"));
-            
-            var javascripts = from filename in new[] { "codemirror.js", "ruby.js", "editor.js" }
-                              select File.ReadAllText(Path.Combine(resourcesDirectoryPath, filename));
-            
-            string javascript = string.Join(Environment.NewLine, javascripts);
-            
-            webView.MainFrame.LoadHtmlString(html.Replace("{{css}}", css).Replace("{{javascript}}", javascript), null);
+            this.webView.MainFrameUrl = "http://localhost:9999/index.html";
         }
         
-        partial void clickedCompile(NSObject sender)
+        void HandleApplicationStopped(object sender, EventArgs e)
         {
-            string code = webView.StringByEvaluatingJavaScriptFromString("return window.editor.getValue();");
-            backgroundWorker.RunWorkerAsync(code);
-            compileButton.Enabled = false;
-            progressIndicator.Hidden = false;
-        }
-
-        void HandleDoWork(object sender, DoWorkEventArgs e)
-        {
-            string code = (string)e.Argument;
-            string randomFileName = Path.GetTempFileName();
-            File.WriteAllText(randomFileName, code);
-            
-            var processStartInfo = new ProcessStartInfo
-            {
-                FileName = "ruby",
-                Arguments = randomFileName,
-                RedirectStandardOutput = true,
-                UseShellExecute = false
-            };
-            
-            using (var ruby = Process.Start(processStartInfo))
-            {
-                var buffer = new StringBuilder();
-                
-                ruby.EnableRaisingEvents = true;
-                ruby.OutputDataReceived += CreateDataReceivedEventHandler(buffer);
-                ruby.BeginOutputReadLine();
-                ruby.WaitForExit();
-                e.Result = buffer.ToString();
-            }
-        }
-        
-        DataReceivedEventHandler CreateDataReceivedEventHandler(StringBuilder buffer)
-        {
-            return (sender, e) =>
-            {
-                buffer.AppendLine(e.Data);
-            };
-        }
-
-        void HandleRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
-            string result = (string)e.Result;
-            NSAlert alert = NSAlert.WithMessage(result, "OK", null, null, "");
-            alert.BeginSheet(Window);
-            progressIndicator.Hidden = true;
-            compileButton.Enabled = true;
+            Close();
         }
     }
 }
